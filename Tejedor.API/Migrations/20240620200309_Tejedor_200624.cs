ec10928b7fe8c6f0069d6a0e95e823ef750b84a4
Albert <albert.gonzalezmoral@gmail.com>
2024-06-20 22:03:48 +0200

    Added migration

---
+++ Tejedor.API/Migrations/20240620200309_Tejedor_200624.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tejedor.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tejedor_200624 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Points",
+                table: "Users",
+                newName: "ACoins");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ACoins",
+                table: "Users",
+                newName: "Points");
+        }
+    }
+}
