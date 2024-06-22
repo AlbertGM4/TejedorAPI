cbcb55274abec9284fa2807dcf46a6a21f6bc35d
Albert <albert.gonzalezmoral@gmail.com>
2024-06-22 22:02:43 +0200

    Added migration

--- Tejedor.API/Migrations/20240622200223_Tejedor_220624_02.Designer.cs
+++ Tejedor.API/Migrations/20240622200223_Tejedor_220624_02.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tejedor.Infrastructure;
 
@@ -11,9 +12,11 @@ using Tejedor.Infrastructure;
 namespace Tejedor.API.Migrations
 {
     [DbContext(typeof(TejedorDBContext))]
-    partial class TejedorDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240622200223_Tejedor_220624_02")]
+    partial class Tejedor_220624_02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Tejedor.API/Migrations/20240622200223_Tejedor_220624_02.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tejedor.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tejedor_220624_02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Total",
+                table: "OrderLines",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Tax",
+                table: "OrderLines",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SubTotal",
+                table: "OrderLines",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Discount",
+                table: "OrderLines",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Total",
+                table: "OrderLines",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Tax",
+                table: "OrderLines",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "SubTotal",
+                table: "OrderLines",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Discount",
+                table: "OrderLines",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+    }
+}
